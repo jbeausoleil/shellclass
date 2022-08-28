04f11ae2fdc519684450b0b471e0db3b5704a86d
Justin Beausoleil <beausoleil.justin@gmail.com>
2022-08-28 16:59:20 -0600

    first commit

---
+++ localusers/luser-demo03.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Display the UID and username of the user executing this script.
+# Display if the user is the vagrant user or not.
+
+# Display the UID
+echo "Your UID is ${UID}"
+
+# Only display if the UID does not match 1000.
+UID_TO_TEST_FOR='1000'
+if [[ "${UID}" -ne "${UID_TO_TEST_FOR}" ]]
+then
+  echo "Your ${UID} does not match ${UID_TO_TEST_FOR}."
+  exit 1
+fi
+
+# Display the username.
+
+# Test if the command succeeded.
+
+# You can use a string test conditional.
+
+# Test for != (not equal) for the string.

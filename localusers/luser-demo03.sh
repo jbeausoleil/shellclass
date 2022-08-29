eb5fe2e95d815385639eb4ed4fd2b4017b66fe56
Justin Beausoleil <beausoleil.justin@gmail.com>
2022-08-28 20:05:31 -0600

    exit statuses, return codes, string test conditionals, more special variables.

--- localusers/luser-demo03.sh
+++ localusers/luser-demo03.sh
@@ -7,7 +7,7 @@
 echo "Your UID is ${UID}"
 
 # Only display if the UID does not match 1000.
-UID_TO_TEST_FOR='1000'
+UID_TO_TEST_FOR='501'
 if [[ "${UID}" -ne "${UID_TO_TEST_FOR}" ]]
 then
   echo "Your ${UID} does not match ${UID_TO_TEST_FOR}."
@@ -15,9 +15,28 @@ then
 fi
 
 # Display the username.
+USER_NAME=$(id -un)
 
 # Test if the command succeeded.
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'The id command did not execute successfully.'
+  exit 1
+fi
+echo "Your username is ${USER_NAME}"
 
 # You can use a string test conditional.
+USER_NAME_TO_TEST_FOR='justinbeausoleil'
+if [[ "${USER_NAME}" = "${USER_NAME_TO_TEST_FOR}" ]]
+then
+  echo "Your username matches ${USER_NAME_TO_TEST_FOR}"
+fi
 
 # Test for != (not equal) for the string.
+if [[ "${USER_NAME}" != "${USER_NAME_TO_TEST_FOR}" ]]
+then
+    echo "Your username does not match ${USER_NAME_TO_TEST_FOR}"
+    exit 1
+fi
+
+exit 0
